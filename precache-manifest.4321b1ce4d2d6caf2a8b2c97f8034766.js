ed6c2982a6dfbb21ce39957c870c0a1a42b833b6
Sarah Chima <sarahchimao@gmail.com>
2019-09-22 23:14:01 +0100

    Updates

---
+++ precache-manifest.4321b1ce4d2d6caf2a8b2c97f8034766.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b623d1dcfd895f94d337318f0515952c",
+    "url": "/sarahchima/countries-app/index.html"
+  },
+  {
+    "revision": "4dff564755e4f8fd17ea",
+    "url": "/sarahchima/countries-app/static/css/main.cf2da6a0.chunk.css"
+  },
+  {
+    "revision": "641cb5fda2c8daa96612",
+    "url": "/sarahchima/countries-app/static/js/2.1e67b78b.chunk.js"
+  },
+  {
+    "revision": "4dff564755e4f8fd17ea",
+    "url": "/sarahchima/countries-app/static/js/main.7fd02fe4.chunk.js"
+  },
+  {
+    "revision": "128726705c2dbe2d1f0f",
+    "url": "/sarahchima/countries-app/static/js/runtime~main.3f8aea65.js"
+  }
+]);
